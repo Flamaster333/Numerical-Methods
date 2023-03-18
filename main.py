99fc8731e16e5507ad26415fc1b5a2cb24c1f8bc
Tysia <tyska.dabrowska@gmail.com>
2023-03-18 02:23:54 +0100

    MACD Formula and EMA Calculating plus drawing a plot

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import csv
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def readCsvFile():
     samples = []
-    rows = []
-#    with open("dis.csv") as file:
-#        csvreader = csv.reader(file)
-#        header = []
-#        header = next(csvreader)
-#        for row in csvreader:
-#            rows.append(row)
-#        file.close()
     data = pd.read_csv("dis.csv")
     data.columns
     samples = data.Zamkniecie
     return samples
 
 
-print(readCsvFile())
+def calcEMA(samples, n):
+    alpha = 2 / (n + 1)
+    ema = samples[0]
+    denominator = 1
+    for i in range(1, n + 1):
+        ema += ((1 - alpha) ** i) * samples[i]
+        denominator += (1 - alpha) ** i
+    return ema / denominator
+
+
+def formulaMACD(samples):
+    ema12 = calcEMA(samples, 12)
+    ema26 = calcEMA(samples, 26)
+
+    return ema12 - ema26
+
+
+samples = readCsvFile()
+samples.plot()
+plt.show()
