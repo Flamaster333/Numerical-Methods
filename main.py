ada5d064e9388a0eafe15f6cf56623f6f0fad83b
Tysia <tyska.dabrowska@gmail.com>
2023-03-19 00:22:08 +0100

    Second plot ready - macd and signal

--- main.py
+++ main.py
@@ -10,25 +10,51 @@ def readCsvFile():
     return samples
 
 
-def calcEMA(samples, n):
+def calcEMA(samples, n, current):
     alpha = 2 / (n + 1)
-    ema = samples[0]
-    denominator = 1
-    for i in range(1, n + 1):
-        ema += ((1 - alpha) ** i) * samples[i]
-        denominator += (1 - alpha) ** i
+    ema = 0
+    denominator = 0
+    for i in range(n + 1):
+        if current - i >= 0:
+            ema += ((1 - alpha) ** i) * samples[current - i]
+            denominator += (1 - alpha) ** i
+        else:
+            break
     return ema / denominator
 
 
-def formulaMACD(samples):
-    ema12 = calcEMA(samples, 12)
-    ema26 = calcEMA(samples, 26)
+def formulaMACD():
+    samples = readCsvFile()
+    macd = []
+    signal = []
+    for current in range(1000):
+        ema12 = calcEMA(samples, 12, current)
+        ema26 = calcEMA(samples, 26, current)
+        macd.append((ema12 - ema26))
+        ema9 = calcEMA(macd, 9, current)
+        signal.append(ema9)
+    drawMainPlot(samples)
+    drawMacdSignalPlot(macd, signal)
+    drawAllInOnePlots(samples, macd, signal)
+    
 
-    return ema12 - ema26
+def drawMainPlot(samples):
+    samples.plot(linewidth='0.8')
+    plt.xlabel("Samples 29.03.2019 - 17.03.2023")
+    plt.ylabel("close of the day on stocks")
+    plt.show()
 
 
-samples = readCsvFile()
-samples.plot(linewidth='0.8')
-plt.xlabel("Samples 29.03.2019 - 17.03.2023")
-plt.ylabel("close of the day on stocks")
-plt.show()
+def drawMacdSignalPlot(macd, signal):
+    plt.plot(macd, linewidth='0.8')
+    plt.plot(signal, linewidth='0.8')
+    plt.xlabel("Samples 29.03.2019 - 17.03.2023")
+    plt.ylabel("close of the day on stocks")
+    plt.show()
+
+
+def drawAllInOnePlots(samples, macd, signal):
+    x = samples
+
+
+formulaMACD()
