0c1d459cd42126b46ae93a131263f1c0cec9ea56
Tysia <tyska.dabrowska@gmail.com>
2023-03-18 01:37:29 +0100

    readCsvFile function

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+import csv
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def readCsvFile():
+    samples = []
+    rows = []
+#    with open("dis.csv") as file:
+#        csvreader = csv.reader(file)
+#        header = []
+#        header = next(csvreader)
+#        for row in csvreader:
+#            rows.append(row)
+#        file.close()
+    data = pd.read_csv("dis.csv")
+    data.columns
+    samples = data.Zamkniecie
+    return samples
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(readCsvFile())
