f21d4af2b108423bfab3ae547ef34f1031953a02
Tysia <tyska.dabrowska@gmail.com>
2023-03-22 00:25:50 +0100

    budget simulation plot

--- Project1_MACD/main.py
+++ Project1_MACD/main.py
@@ -31,13 +31,16 @@ def formulaMACD():
     samples = readCsvFileClosure()
     macd = []
     signal = []
-    for current in range(50):
+    for current in range(1000):
         ema12 = calcEMA(samples, 12, current)
         ema26 = calcEMA(samples, 26, current)
         macd.append((ema12 - ema26))
         ema9 = calcEMA(macd, 9, current)
         signal.append(ema9)
-    drawPlots(samples, macd, signal)
+    budget_plot = []
+    budget_plot = intersectionSignalMacd(samples, macd, signal)
+    drawPlots(samples, macd, signal, budget_plot)
+
 
 
 def showPlot():
@@ -48,7 +51,7 @@ def showPlot():
 
 
 # all plot creating
-def drawPlots(samples, macd, signal):
+def drawPlots(samples, macd, signal, budget):
     # main data plot
     plt.figure().set_figwidth(15)
     plt.plot(samples, color='g', linewidth='0.8')
@@ -64,10 +67,20 @@ def drawPlots(samples, macd, signal):
     plt.plot(macd, color='b', linewidth='0.8')
     plt.plot(signal, color='r', linewidth='0.8')
     showPlot()
+    # all in one plot
+    plt.figure().set_figwidth(15)
+    plt.plot(samples, color='g', linewidth='0.8')
+    plt.plot(macd, color='b', linewidth='0.8')
+    plt.plot(signal, color='r', linewidth='0.8')
+    plt.plot(budget, color='y', linewidth='0.8')
+    showPlot()
 
 
-def intersectionSignalMacd(macd, signal):
-    macd_was_higher = 1
+def intersectionSignalMacd(samples, macd, signal):
+    if macd[34] > signal[34]:
+        macd_was_higher = 1
+    else:
+        macd_was_higher = 0
     intersection_values = []
 
     for i in range(35, 1000):
@@ -77,12 +90,30 @@ def intersectionSignalMacd(macd, signal):
         elif macd_was_higher == 0 and macd[i] >= signal[i]:
             intersection_values.append((i, macd_was_higher))
             macd_was_higher = 1
-    return intersection_values
+    return simulation(samples, intersection_values)
 
 
 def simulation(samples, intersection_values):
-    budget = 1000
-    
+    budget = 500
+    budget_plot = []
+    for i in range(0, intersection_values[0][0]):
+        budget_plot.append(budget)
+    print("Budget at the beginning: ", budget, "$")
+    number_of_shares = 0
+    if intersection_values[(len(intersection_values)-1)][1] == 0:
+        intersection_values.pop(len(intersection_values)-1)
+    for intersection in intersection_values:
+        if intersection[1] == 0:
+            number_of_shares = budget // samples[intersection[0]]
+            budget -= (number_of_shares * samples[intersection[0]])
+        else:
+            budget += (number_of_shares * samples[intersection[0]])
+            number_of_shares = 0
+        if intersection[0] != intersection_values[(len(intersection_values)-1)][0]:
+            for i in range(intersection[0], intersection_values[intersection_values.index(intersection)+1][0]):
+                budget_plot.append(budget)
+    print("Budget at the end: ", budget, "$")
+    return budget_plot
 
 
 # start
